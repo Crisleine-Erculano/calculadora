830deb1de4dde2f5f4a975071de2df5a1570393f
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-02-24 13:55:46 -0300

    Criando função de apagar 1 item por vez

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-let entrada = '';
-function adicionar(numero){
-   
-      entrada = document.querySelector('#entrada').innerHTML;
+
+function adicionar(numero) {
+      let entrada = document.querySelector('#entrada').innerHTML;
       document.querySelector('#entrada').innerHTML = entrada + numero;
-  
+      
 }
-function limpar(){
-   entrada =''
-   document.querySelector('#entrada').innerHTML = entrada;
+function limpar() {
+      let entrada = '';
+      document.querySelector('#entrada').innerHTML = entrada;
+}
+function limparElemento(){
+      let entrada = document.getElementById('entrada').innerHTML;
+      document.getElementById('entrada').innerHTML = entrada.substring(0, entrada.length -1);
 }
 
-
+ 
