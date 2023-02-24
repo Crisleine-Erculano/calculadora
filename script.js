d9ed6e5862b7429d4fa6432e647bb61db2561dec
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-02-24 14:27:27 -0300

    Criando a função de calcular operações matemáticas

--- script.js
+++ script.js
@@ -9,8 +9,15 @@ function limpar() {
       document.querySelector('#entrada').innerHTML = entrada;
 }
 function limparElemento(){
-      let entrada = document.getElementById('entrada').innerHTML;
-      document.getElementById('entrada').innerHTML = entrada.substring(0, entrada.length -1);
+      let limpar  =  document.querySelector( '#entrada' ).innerHTML;
+      document.querySelector( '#entrada' ).innerHTML= limpar.substring ( 0 ,  limpar.length  - 1 ) ;
 }
 
- 
+function resultado(){
+      let calcula = document.querySelector('#entrada').innerHTML;
+      if(calcula){
+                document.querySelector( '#entrada' ).innerHTML  =  eval ( calcula ) ;
+            }
+      }
+       
+       
